9df7efe2bd8c5760fbe5ef1f658fa0475e4d1144
amarcheluk <amarnathcs1995@gmail.com>
2023-11-30 16:41:59 +0530

    1St commit

---
+++ src/test/java/demo/Demo1Test.java
@@ -0,0 +1,15 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class Demo1Test {
+	@Test
+	public void demo1test() {
+		System.out.println("-hi------");
+	}
+	
+	public void demo12est() {
+		System.out.println("-------hello------");
+	}
+
+}
---
+++ src/test/java/demo/Demo2Test.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class Demo2Test {
+	public void demo3test() {
+		System.out.println("-bye-----");
+	}
+	
+	public void demo4est() {
+		System.out.println("-------tata------");
+	}
+
+
+}
